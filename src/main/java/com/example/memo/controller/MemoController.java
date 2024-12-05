31ee9af4fbf2f0c8bb0890ed6d345b531b9a9131
고수연 <sy@gosuyeon-ui-MacBookPro.local>
2024-12-05 14:07:20 +0900

    메모 목록 조회

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -3,11 +3,11 @@ package com.example.memo.controller;
 import com.example.memo.dto.MemoRequestDto;
 import com.example.memo.dto.MemoResponseDto;
 import com.example.memo.entity.Memo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/memos")
@@ -17,7 +17,8 @@ public class MemoController {
 
     // 메모 생성 기능
     @PostMapping
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+    public ResponseEntity<MemoResponseDto> createMemo(@RequestBody MemoRequestDto requestDto) {
+        // ResponseEntity : 상태 코드 반환
         // @RequestBody : 클라이언트로부터 JSON파일을 요청받았을 때, 파라미터로 바로 바인딩
 
 
@@ -30,7 +31,37 @@ public class MemoController {
         // Inmemory DB에 Memo 메모
         memoList.put(memoId, memo);
 
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.CREATED);
+    }
+
+// 메모 전체 목록 조회 기능
+    /*
+    - 메모 전체 목록을 조회할 수 있다. (READ)
+    - 여러개의 데이터를 배열 형태로 한번에 응답한다.
+    - 데이터가 없는 경우 비어있는 배열 형태로 응답한다.
+    - 응답 상태코드는 200 OK로 설정한다.
+     */
+
+    @GetMapping
+    public ResponseEntity<List<MemoResponseDto>> findAllMemos(){
+        // init List
+        List<MemoResponseDto> responseList = new ArrayList<>();
+
+        // HashMap<Memo> -> List<MemoResponseDto> 형태로
+        for(Memo memo : memoList.values()) {
+            MemoResponseDto responseDto = new MemoResponseDto(memo);
+            responseList.add(responseDto);
+        }
+
+        // Map To List
+//        responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
+
+        // 데이터가 없으면 204 No Content, 있으면 200 OK
+        if (responseList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204 No Content
+        } else {
+            return new ResponseEntity<>(responseList, HttpStatus.OK); // 200 OK
+        }
     }
 
     // 메모 조회 기능
@@ -42,6 +73,10 @@ public class MemoController {
 
         return new MemoResponseDto(memo);
     }
+
+
+
+
 
     // 메모 수정 기능
     @PutMapping("/{id}")
